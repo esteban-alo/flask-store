e40dada3047df32c92bdea728abf3eada9ffd473
Esteban Rodriguez Alonso <esteban@iMac-de-Esteban.local>
2022-03-29 11:46:00 -0500

    refactor: improve services responses

--- app/api/users_management/users/services.py
+++ app/api/users_management/users/services.py
@@ -1,37 +1,51 @@
-import json
+from typing import List
+
 from injector import inject
+
+from app.api.commons.commons import (
+    error_response,
+    get_current_datetime,
+    uuid,
+)
 from .interfaces import (
     User,
     UsersResponse,
 )
-from app.api.commons.commons import error_response
-from .schemas import ErrorSchema
 
 
 class UserService:
-    users_list: list[User]
+    id: int
+    users_list: List[User]
 
     @inject
     def __init__(self):
+        self.id = 0
         self.users_list = []
 
     def create_user(self, user: User) -> None:
         """ Get users data by username """
+        user.id = self.id
+        user.uuid = uuid()
+        user.created_at = get_current_datetime()
+        user.updated_at = get_current_datetime()
+        self.id = self.id + 1
         self.users_list.append(user)
 
     def get_users(self):
-        paginated_result = {
-            "limit": 0,
-            "offset": 0,
-            "total": len(self.users_list),
-            "results": json.loads(json.dumps(self.users_list, default=lambda x: x.__dict__))
-        }
-        return paginated_result
+        response = UsersResponse.schema()
+        result = UsersResponse(
+            limit=0,
+            offset=0,
+            total=len(self.users_list),
+            results=self.users_list
+        )
+        return response.dump(result)
 
     def get_user_data(self, name: str) -> User:
         """ Get users data by username """
-        for user in self.users_list:
-            return user.to_dict()
+        user_schema = User.schema()
+        for item in self.users_list:
+            if item.name == name:
+                return user_schema.dump(item)
         return error_response("E01", f"{name} object not Found", "Fields Validation Error",
-                               "/users_management/users/", 404)
-
+                              "/users_management/users/", 404)
